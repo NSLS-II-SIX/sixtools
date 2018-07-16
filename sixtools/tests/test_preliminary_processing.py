998eb48691edaadd1a2618641828adbaedfc77d1
Andrew Walter <awalter@bnl.gov>
2018-07-16 13:18:19 -0400

    modified test_preliminary_processing to ensure that the inputted data is a generator

--- sixtools/tests/test_preliminary_processing.py
+++ sixtools/tests/test_preliminary_processing.py
@@ -3,14 +3,15 @@ import numpy as np
 from sixtools import preliminary_processing as pre_proc
 
 
-@pytest.fixture
-def data():
-    # generate a list of 2D numpy arrays.
-    out_list = []
-    for i in range(0, 3):
-        out_list.append(np.arange(1, 26).reshape(5, 5))
+def create_raw_data():
+    # create a generator of 2D numpy aray lists.
+    for j in range(0,3):
+        # generate a list of 2D numpy arrays.
+        out_list = []
+        for i in range(0, 3):
+            out_list.append(np.arange(1, 26).reshape(5, 5))
 
-    yield out_list
+        yield out_list
 
 
 @pytest.fixture
@@ -35,11 +36,13 @@ def expected_extracted_regions():
     return out_dict
 
 
-def test_extract_region(data, regions, expected_extracted_regions):
+def test_extract_region(regions, expected_extracted_regions):
     # test extract_region.
+    raw_data=create_raw_data() #this is done in this way as I need a
+                               #generator object.
     for region in regions:
         expecteds = expected_extracted_regions[region]
-        found = next(pre_proc.extract_region(data, region, regions))
+        found = next(pre_proc.extract_region(raw_data, region, regions))
         for i in range(0, len(expecteds)):
             expected = expecteds[i]
             actual = found[i]
