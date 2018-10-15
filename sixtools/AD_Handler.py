c38f92e5f701a7db006fcc5c52a6e07b810f00f6
Andrew Walter <awalter@bnl.gov>
2018-10-15 14:10:14 -0400

    Making a change so that the handler takes in column names as defined in resource_kwargs

--- sixtools/AD_Handler.py
+++ sixtools/AD_Handler.py
@@ -4,23 +4,12 @@ import pandas as pd
 import numpy as np
 
 
-class HDF5SingleHandler_centroid7xn(HandlerBase):
-    '''Handler for hdf5 data stored 1 image per file by the rixscam xip plugin.
+class AreaDetector_HDF5SingleHandler_DataFrame(HandlerBase):
+    '''Handler for hdf5 data stored 1 image per file and returned as a
+    Pandas.DataFrame.
 
-    This will work with all hdf5 files that are a 7xn array where n is the
-    number of centroid 'events' and the 7 'columns' have the format:
-
-        column 1: x
-        column 2: y
-        column 3: x eta correction
-        column 4: y eta correction
-        column 5: y eta correction & isolinear correction
-        column 6: "sum of 3x3 region" (though, this could also mean 2x2 region)
-        column 7: XIP mode (which I assume to be either 3x3 or 2x2)
-
-    This handler will ouput the data as a pandas dataframe with the 'fields'
-    (in order): `x`, `y`, `x_eta`, `y_eta`, `y_eta_iso`, `sum regions`,
-    XIP_mode.
+    This will work with all hdf5 files that are a mxn arrays and the data is
+    'table like' where m is the number of columns and n is the number of rows.
 
     Parameters
     ----------
@@ -35,16 +24,18 @@ class HDF5SingleHandler_centroid7xn(HandlerBase):
     frame_per_point : float
         the number of frames per point.
     '''
-    specs = {'AD_HDF5_SINGLE_C7xn'} | HandlerBase.specs
+    specs = {'AD_HDF5_SINGLE_DATAFRAME'} | HandlerBase.specs
 
-    def __init__(self, fpath, template, filename, key, frame_per_point=1):
+    def __init__(self, fpath, template, filename, key='/entry/data/data',
+                 column_names=None, frame_per_point=1):
+        # I have included defaults for `key` and 'column_names' for back
+        # compatibility with existing files at SIX.
         self._path = os.path.join(fpath, '')
         self._fpp = frame_per_point
         self._template = template
         self._filename = filename
         self._key = key
-        self._column_names = ['x', 'y', 'x_eta', 'y_eta', 'y_eta_iso',
-                              'sum_regions', 'XIP_mode']
+        self._column_names = column_names
 
     def _fnames_for_point(self, point_number):
         start = int(point_number * self._fpp)
@@ -67,41 +58,3 @@ class HDF5SingleHandler_centroid7xn(HandlerBase):
         for d_kw in datum_kwargs:
             ret.extend(self._fnames_for_point(**d_kw))
         return ret
-
-
-class AreaDetectorHDF5SingleHandler_centroid7xn(HDF5SingleHandler_centroid7xn):
-    '''Handler for hdf5 data stored 1 image per file by areadetector from the
-    rixscam xip plugin.
-
-
-    This will work with all hdf5 files that are a 7xn array where n is the
-    number of centroid 'events' and the 7 'columns' have the format:
-
-        column 1: x
-        column 2: y
-        column 3: x eta correction
-        column 4: y eta correction
-        column 5: y eta correction & isolinear correction
-        column 6: "sum of 3x3 region" (though, this could also mean 2x2 region)
-        column 7: XIP mode (which I assume to be either 3x3 or 2x2)
-
-    This handler will ouput the data as a pandas dataframe with the 'fields'
-    (in order): `x`, `y`, `x_eta`, `y_eta`, `y_eta_iso`, `sum regions`,
-    XIP_mode.
-
-    Parameters
-    ----------
-    fpath : string
-        filepath
-    template : string
-        filename template string.
-    filename : string
-        filename
-    frame_per_point : float
-        the number of frames per point.
-    '''
-    def __init__(self, fpath, template, filename, frame_per_point=1):
-        hardcoded_key = '/entry/data/data'
-        super(AreaDetectorHDF5SingleHandler_centroid7xn, self).__init__(
-              fpath=fpath, template=template, filename=filename,
-              key=hardcoded_key, frame_per_point=frame_per_point)
