551859e129e441694c7931f25e68becb4fe3e4e0
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-11-07 14:50:58 +0000

    Update docs after building Travis build 76 of NSLS-II-SIX/sixtools
    The docs were built from the branch 'master' against the commit
    c545a58a853733eef086de25084cf1790b861d84.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/NSLS-II-SIX/sixtools/jobs/451922270.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post55+g4f97591',
+    VERSION: '0.0.1.post4+gc545a58',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
